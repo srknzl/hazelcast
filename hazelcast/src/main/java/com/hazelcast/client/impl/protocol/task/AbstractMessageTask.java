2156b6ef2d254aa63982301c7cc418f6da5e2ebf
Serkan Özel <serkan.ozel@hazelcast.com>
2022-11-16 19:12:27 +0300

    Dont close connection on auth failure

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/task/AbstractMessageTask.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/task/AbstractMessageTask.java
@@ -168,21 +168,21 @@ public abstract class AbstractMessageTask<P> implements MessageTask, SecureReque
 
     private void handleAuthenticationFailure() {
         Exception exception;
-        String closeReason;
+        // String closeReason;
         if (nodeEngine.isRunning()) {
             String message = "Client " + endpoint + " must authenticate before any operation.";
             String secureMessage = "Client " + endpoint.toSecureString() + " must authenticate before any operation.";
 
             logger.severe(message);
 
-            closeReason = message;
+            // closeReason = message;
             exception = new RetryableHazelcastException(new AuthenticationException(secureMessage));
         } else {
             exception = new HazelcastInstanceNotActiveException();
-            closeReason = exception.getMessage();
+            // closeReason = exception.getMessage();
         }
         sendClientMessage(exception);
-        connection.close("Authentication failed. " + closeReason, null);
+        // connection.close("Authentication failed. " + closeReason, null);
     }
 
     private void logProcessingFailure(Throwable throwable) {
