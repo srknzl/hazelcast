e7f3eafedb95585124a054a2ef549ee41e94d124
Serkan Özel <serkan.ozel@hazelcast.com>
2023-05-08 22:54:03 +0300

    Read write wan related stuff

--- hazelcast/src/main/java/com/hazelcast/map/impl/operation/PublishWanEventOperation.java
+++ hazelcast/src/main/java/com/hazelcast/map/impl/operation/PublishWanEventOperation.java
@@ -1,22 +1,26 @@
 package com.hazelcast.map.impl.operation;
 
+import com.hazelcast.internal.nio.IOUtil;
 import com.hazelcast.map.impl.MapDataSerializerHook;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
-import com.hazelcast.spi.impl.operationservice.Operation;
 import com.hazelcast.spi.impl.operationservice.PartitionAwareOperation;
-import com.hazelcast.wan.WanEvent;
 import com.hazelcast.wan.WanPublisher;
+import com.hazelcast.wan.impl.InternalWanEvent;
+
+import java.io.IOException;
 
 
-public class PublishWanEventOperation extends Operation implements PartitionAwareOperation, IdentifiedDataSerializable {
+public class PublishWanEventOperation extends MapOperation implements PartitionAwareOperation, IdentifiedDataSerializable {
     private String wanReplicationName;
     private String wanPublisherId;
-    private WanEvent wanEvent;
+    private InternalWanEvent wanEvent;
 
     public PublishWanEventOperation() {
     }
 
-    public PublishWanEventOperation(String wanReplicationName, String wanPublisherId, int partitionId, WanEvent wanEvent) {
+    public PublishWanEventOperation(String wanReplicationName, String wanPublisherId, int partitionId, InternalWanEvent wanEvent) {
         this.wanReplicationName = wanReplicationName;
         this.wanPublisherId = wanPublisherId;
         this.wanEvent = wanEvent;
@@ -24,7 +28,7 @@ public class PublishWanEventOperation extends Operation implements PartitionAwar
     }
 
     @Override
-    public void run() throws Exception {
+    protected void runInternal() {
         WanPublisher publisher = this.getNodeEngine().getWanReplicationService().getPublisherOrFail(this.wanReplicationName, this.wanPublisherId);
         publisher.publishReplicationEvent(wanEvent);
     }
@@ -37,5 +41,21 @@ public class PublishWanEventOperation extends Operation implements PartitionAwar
     @Override
     public int getClassId() {
         return MapDataSerializerHook.MAP_WAN_PUBLISH_EVENT;
+    }
+
+    @Override
+    protected void writeInternal(ObjectDataOutput out) throws IOException {
+        super.writeInternal(out);
+        out.writeObject(wanEvent);
+        out.writeString(wanReplicationName);
+        out.writeString(wanPublisherId);
+    }
+
+    @Override
+    protected void readInternal(ObjectDataInput in) throws IOException {
+        super.readInternal(in);
+        wanEvent = in.readObject();
+        wanReplicationName = in.readString();
+        wanPublisherId = in.readString();
     }
 }
